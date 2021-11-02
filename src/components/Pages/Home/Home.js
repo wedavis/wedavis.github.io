53d98cca1d09bddc8c36bb460b2c8d41779f982a
wedavis <wedavis2@ncsu.edu>
2021-11-02 16:48:51 -0400

    modified homepage

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -4,14 +4,6 @@ function Home() {
     <div className="Home">
      <h1 className="initial-header">William Davis</h1>
       <header className="Home-header">
-        <a
-          className="Home-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   )
